8bafb7bf13b40ee5d8bfa6652399b84c01fb84dc
Danielle <43252788+Danielle-D@users.noreply.github.com>
2021-11-14 10:11:10 +0000

    Add files via upload

---
+++ MinimalAPI/MinimalApiApplication/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.MapGet("/", () => "Hello World!");
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("WeatherForecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.UseSwaggerUI();
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ MinimalAPI/MinimalApiApplication/ReadMe.md
@@ -0,0 +1,3 @@
+﻿This is a .Net 6 minimal API with swagger documentation.
+
+Run the application, to open swagger navigate to: https://localhost:7058/swagger/index.htm
